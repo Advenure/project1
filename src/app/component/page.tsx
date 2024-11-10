40c99c1096c696328ff0e81f8ea972051b5fa927
kamran tasleem Ahmed <ccodinglover@gmail.com>
2024-11-10 20:09:15 +0500

    project

--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -1,7 +1,6 @@
 
 import Link from "next/link"
 import Image from "next/image"
-import Aboutus from "../about/page"
 
 
 export default function Header(){
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 
 
-import Aboutus from "./about/page";
 import Section1 from "./herosection1/page";
 import Section2 from "./herosection2/page";
 
